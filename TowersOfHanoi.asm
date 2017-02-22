5fd409f7f76503d5efb0b02308b9caa17577453b
Joseph Lyons <josephlyons@josephs-air.pk5001z>
2017-02-22 01:46:58 -0500

    Finished with main, next is the recursive aspect

--- TowersOfHanoi.asm
+++ TowersOfHanoi.asm
@@ -16,15 +16,27 @@ newline:       .asciiz "\n"
 .globl main
 
 # Variable Key:
-# $s0 = n
+# $a0 = n
+# $a1 = start
+# $a2 = finish
+# $a3 = extra
 
 main: # main program entry
 
-    addi $v0, $zero, 4     # Load number for system call for printing string
-    la   $a0, requestInput # Load string
-    syscall
+       addi $v0, $zero, 4     # Load number for system call for printing string
+       la   $a0, requestInput # Load string
+       syscall
     
-    addi $v0, $zero, 5     # Load number for system call for reading number in
-    syscall
-     
+       addi $v0, $zero, 5     # Load number for system call for reading number in
+       syscall
+    
+       addi $a0, $v0,   0     # Put user number in $a0
+       addi $a1, $zero, 1
+       addi $a2, $zero, 2
+       addi $a3, $zero, 3
+    
+       jal hanoi
+
+hanoi: 
+
       ### Double check that I'm using good register conventions
