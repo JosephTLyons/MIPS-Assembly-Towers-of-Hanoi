0767e08098f46d73f2d1f17e0be75ba07cb29fd7
Joseph Lyons <josephlyons@josephs-air.pk5001z>
2017-02-22 01:37:40 -0500

    Starting to build the main function

--- TowersOfHanoi.asm
+++ TowersOfHanoi.asm
@@ -6,13 +6,25 @@
 ################# Data segment #####################
 .data
 
-outputText:  .asciiz " is a prime number.\n"
+requestInput:  .asciiz "Enter number of disks: "
+moveDisk:      .asciiz "Move disk "
+fromPeg:       .asciiz "From peg "
+toPeg:         .asciiz "To peg "
+newline:       .asciiz "\n"
 ################# Code segment #####################
 .text
 .globl main
 
+# Variable Key:
+# $s0 = n
+
 main: # main program entry
 
-            
+    addi $v0, $zero, 4     # Load number for system call for printing string
+    la   $a0, requestInput # Load string
+    syscall
+    
+    addi $v0, $zero, 5     # Load number for system call for reading number in
+    syscall
      
       ### Double check that I'm using good register conventions
