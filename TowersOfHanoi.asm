9d679f3a4d50c767fbefdb444728922a677042ad
Joseph Lyons <josephlyons@Josephs-Air.PK5001Z>
2017-02-22 01:15:32 -0500

    Initial Commit

---
+++ TowersOfHanoi.asm
@@ -0,0 +1,18 @@
+# Title: Programming Project Part 3		#Filename: TowersOfHanoi.asm
+# Author: Joseph Lyons				#Date: February 22th, 2017
+# Description: Displays the moves needed to solve the puzzle of the towers of Hanoi
+# Input: Number of rings
+# Output: Steps for solving the problem
+################# Data segment #####################
+.data
+
+outputText:  .asciiz " is a prime number.\n"
+################# Code segment #####################
+.text
+.globl main
+
+main: # main program entry
+
+            
+     
+      ### Double check that I'm using good register conventions
