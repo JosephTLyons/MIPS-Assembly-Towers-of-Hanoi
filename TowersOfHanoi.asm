b5e980bcd5c3475a7c429286f275dea99b39ca04
Joseph Lyons <josephlyons@josephs-air.pk5001z>
2017-02-23 00:49:35 -0500

    Still working on the logic for this program

--- TowersOfHanoi.asm
+++ TowersOfHanoi.asm
@@ -17,12 +17,14 @@ newline:       .asciiz "\n"
 
 # Variable Key:
 # $a0 = n
-# $a1 = start
-# $a2 = finish
-# $a3 = extra
-# $t0 = 0 or 1 in slti instruction in Hanoi function
+# $s1 = $a1 = start  (1)
+# $s2 = $a2 = finish (2)
+# $s3 = $a3 = extra  (3)
 
-main: # main program entry
+main:
+       addi $s1, $zero, 1     # Storing start
+       addi $s2, $zero, 2     # Storing finish
+       addi $s3, $zero, 3     # Stroing extra
 
        addi $v0, $zero, 4     # Load number for system call for printing string
        la   $a0, requestInput # Load string
@@ -32,15 +34,16 @@ main: # main program entry
        syscall
     
        addi $a0, $v0,   0     # Put user number in $a0
-       addi $a1, $zero, 1
-       addi $a2, $zero, 2
-       addi $a3, $zero, 3
+       add  $a1, $s1, $zero   # Put 1 in $a1
+       add  $a2, $s2, $zero   # Put 2 in $a2
+       add  $a3, $s3, $zero,  # Put 3 in $a3
     
-       j hanoi
+       jal hanoi
+Exit:  
 
-hanoi: addi $sp, $sp, -8     # Make room in stack
+hanoi: addi $sp, $sp, -8      # Make room in stack
+       sw   $ra, 4($sp)       # Store returning address
        sw   $a0, 0($sp)       # Store n
-       sw   $ra, 4($sp)      # Store returning address
 
        slti $t0, $a0, 1       # Test base case
        beq  $t0, $zero, Skip
@@ -49,20 +52,22 @@ hanoi: addi $sp, $sp, -8     # Make room in stack
 
 Skip:  addi $a0, $a0, -1      # Decrement n
        jal hanoi              
-       lw   $ra, 4($sp)      # Load returning address
        lw   $a0, 0($sp)       # Load n
-       addi $sp, $sp, 8      # Make room in stack
+       lw   $ra, 4($sp)       # Load returning address
+       addi $sp, $sp, 8       # Adjust stack pointer
        
        
        ########
        #Code for printing and dispaying numbers
+       
+       add $t0, $a0, $zero    # Back up n temporarily
        
        addi $v0, $zero, 4     # Load number for system call for printing string
        la   $a0, moveDisk     # Load string
        syscall
     
        addi $v0, $zero, 1     # Load number for system call for print number
-       add  $a0, $zero, $a1   # Move $a0 for printing
+       add  $a0, $t0, $zero   # Move $a0 for printing
        syscall
        
        addi $v0, $zero, 4     # Load number for system call for printing string
@@ -70,7 +75,7 @@ Skip:  addi $a0, $a0, -1      # Decrement n
        syscall
     
        addi $v0, $zero, 1     # Load number for system call for print number
-       add  $a0, $zero, $a2   # Move $a1 for printing
+       add  $a0, $zero, $a1   # Move $a1 for printing
        syscall
        
        addi $v0, $zero, 4     # Load number for system call for printing string
@@ -78,15 +83,22 @@ Skip:  addi $a0, $a0, -1      # Decrement n
        syscall
     
        addi $v0, $zero, 1     # Load number for system call for print number
-       add  $a0, $zero, $a3   # Move $a2 for printing
+       add  $a0, $zero, $a2   # Move $a2 for printing
        syscall
        
        addi $v0, $zero, 4     # Load number for system call for printing string
-       la   $a0, newline        # Load string
+       la   $a0, newline      # Load string
        syscall
        
        #########
        
+       add  $a0, $t0, $zero   # recover n
+       
+       addi $a0, $a0, -1      # Decrement n
+       add  $a1, $s2, $zero   # Put 2 in $a1
+       add  $a2, $s3, $zero   # Put 3 in $a2
+       add  $a3, $s1, $zero   # Put 1 in $a3
+       j hanoi
        
        jr $ra
 
@@ -94,3 +106,4 @@ Skip:  addi $a0, $a0, -1      # Decrement n
 Leave:
 
       ### Double check that I'm using good register conventions
+      ### Clean up commments and whitespace, remove ###s
